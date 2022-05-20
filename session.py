9248732ee71a026653fa360df5c952514ef3e13d
Vladyslav Miliev <vladyslavmi@nayax.com>
2022-05-21 00:15:09 +0300

    created

---
+++ session.py
@@ -0,0 +1,154 @@
+from sqlalchemy.orm import sessionmaker
+from DAO import engine
+
+
+class Session:
+    """
+    Class needed to create a session instance
+    """
+
+    def __init__(self):
+        self.engine = engine
+        self.maker = sessionmaker(self.engine)
+        self.session = self.maker()
+
+    def query(self):
+        """
+        :return: session instance
+        """
+        return self.session
+
+    def row2dict(self, row):
+        """
+        Convert SQLAlchemy row object to a Python dict
+        :param row: object to generate
+        :return: dict with neat object output
+        """
+        d = {}
+        for column in row.__table__.columns:
+            d[column.name] = str(getattr(row, column.name))
+
+        return d
+
+    def list2dict(self, lst):
+        """
+        Convert SQLAlchemy row object to a Python dict
+        :param lst: object to generate
+        :return: list dict with neat object output
+        """
+        d = {}
+        l = []
+        for item in range(len(lst)):
+            for column in lst[item].__table__.columns:
+                d[column.name] = str(getattr(lst[item], column.name))
+            l.append(d)
+        return l
+
+    def check_fields_for_update(self, instance, data):
+        """
+        The method compares the parameters of the updated object with the existing one.
+        Leaves the current data and changes the new ones
+        :param instance: instance of query result
+        :param data: parameters to be updated
+        :return: new instance
+        """
+        for i in self.row2dict(instance).keys():
+            if self.row2dict(instance).get(i) != data.get(i):
+                setattr(instance, i, data.get(i))
+
+        return instance
+
+
+class SessionQueryCreate(Session):
+    """
+    Class decorator
+    Needed to decorate all transaction function which post some data like: create_user()
+    After triggering, adds an instance of the DAO class to the session, commits and closes the session
+    Example:
+    @SessionQueryPost
+    def create_user(name)
+        return User(name)
+    """
+
+    def __init__(self, function):
+        super().__init__()
+        self.function = function
+
+    def __call__(self, data: dict):
+        try:
+            instance = self.function(self, data)
+            self.session.add(instance)
+            self.session.commit()
+            return data
+        except Exception as error:
+            return {'error': error}
+        finally:
+            self.session.close()
+
+
+class SessionQueryGet(Session):
+    """
+    Class decorator
+    Needed to decorate all transaction function which get some data like: create_user()
+    After triggering, adds an instance of the DAO class to the session, commits and closes the session
+    Example:
+    @SessionQueryGet
+    def get_user_by_id(id)
+        return self.session.query(User).filter(User.id == id).first()
+    """
+
+    def __init__(self, function):
+        super().__init__()
+        self.function = function
+
+    def __call__(self, data: dict = None):
+        try:
+            instance = self.function(self, data)
+            return self.row2dict(instance)
+        except Exception as error:
+            return {'error': error}
+        finally:
+            self.session.close()
+
+
+class SessionQueryGetAll(Session):
+    """
+    Class decorator
+    Needed to decorate all transaction function which get some data like: create_user()
+    After triggering, adds an instance of the DAO class to the session, commits and closes the session
+    Example:
+    @SessionQueryGet
+    def get_user_by_id(id)
+        return self.session.query(User).filter(User.id == id).first()
+    """
+
+    def __init__(self, function):
+        super().__init__()
+        self.function = function
+
+    def __call__(self, data: dict = None):
+        try:
+            instance = self.function(self)
+            return self.list2dict(instance)
+        except Exception as error:
+            return {'error': error}
+        finally:
+            self.session.close()
+
+
+class SessionQueryUpdate(Session):
+
+    def __init__(self, function):
+        super().__init__()
+        self.function = function
+
+    def __call__(self, data: dict = None):
+        try:
+            instance = self.function(self, data)
+            self.check_fields_for_update(instance, data)
+            self.session.commit()
+            return self.row2dict(instance)
+        except Exception as error:
+            return {'error': error}
+        finally:
+            self.session.close()
